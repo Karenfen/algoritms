29b8a41c8e9e4ed6baac8512cd04d5dbcd243beb
Ilya_Kiselev <mybutifullie@gmail.com>
2021-07-15 11:08:39 +1000

    created Hello,world and first_algoritm

---
+++ lesson-2/hello_world.cpp
@@ -0,0 +1,15 @@
+// Kiselev Ilia
+// created from VS code + MinGW
+// Task 2
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+string name;
+cout << "inter your name: ";
+cin >> name;
+cout << "Hello, " << name << "!" << endl;
+    return 0;
+}
