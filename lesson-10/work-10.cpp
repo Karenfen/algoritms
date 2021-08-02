97428ef986aa31a23f3f78cc78b016ae12df402b
Ilya_Kiselev <mybutifullie@gmail.com>
2021-08-02 19:55:38 +1000

    added functions

--- lesson-10/work-10.cpp
+++ lesson-10/work-10.cpp
@@ -3,11 +3,58 @@
 #include <cstdlib>
 #include <iostream>
 
+#define Q int
 using namespace std;
+
+typedef struct Node {
+    Q date;
+    struct Node *next;
+} Node;
+
+typedef struct List {
+    Node *head;
+    int size;
+} List;
+
+void initList (List *lst){
+    lst->head = NULL;
+    lst->size = 0;
+}
+
+void insertNode (List *lst, Q data){
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->date = data;
+    newNode->next = NULL;
+
+    if (lst->head == NULL){
+        lst->head = newNode;
+        lst->size++;
+        return;
+    }
+
+    Node *current = lst->head;
+
+    while (current->next != NULL){
+        current = current->next;
+    }
+    current->next = newNode;
+    lst->size++;
+}
+
+Node *removeNode (List *lst, Q val){
+    
+}
+void printList (List *lst){
+
+}
 
 // Task-1 ====================================
 
 // Task-2 ====================================
+
+void copyList(Node* list){
+
+}
 
 // Task-3 ====================================
 
