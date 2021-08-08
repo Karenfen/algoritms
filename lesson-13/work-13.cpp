dcab0d6c4a3fc16d71288c0aaf7b4e72c7da14cb
Ilya_Kiselev <mybutifullie@gmail.com>
2021-08-08 21:22:34 +1000

    created work-13.cpp

---
+++ lesson-13/work-13.cpp
@@ -0,0 +1,27 @@
+// Author - Ilia Kiselev
+
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main (const int argc, const char** argv) {
+// TASK-1 ====================================
+cout << "TASK-1 ====================================" << endl << endl;
+
+
+
+// TASK-2 ====================================
+cout << "TASK-2 ====================================" << endl << endl;
+
+
+
+// TASK-3 ====================================
+cout << "TASK-3 ====================================" << endl << endl;
+
+
+
+system("pause");
+return 0;
+}
